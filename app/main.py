c08a3ba81f99b601ae4a8d4f1ad056123ccefe36
luthfibalaka <luthfibalaka@gmail.com>
2023-05-26 17:11:14 +0700

    fix(api key): fix api key error

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 async def all_ehr_data():
     res = httpx.get(
         "http://34.171.251.150:3000/api/assets",
-        headers={"x-api-key": "0e7800c7-9632-49a5-8170-151b010e4655"},
+        headers={"x-api-key": "4973b1ac-5e6b-4379-aa31-4a8ff80ba398"},
     )
     return {"all ehrs data": res.json()}
 
@@ -17,6 +17,6 @@ async def all_ehr_data():
 async def ehr_history(ehr_id):
     res = httpx.get(
         f"http://34.171.251.150:3000/api/assets/{ehr_id}",
-        headers={"x-api-key": "0e7800c7-9632-49a5-8170-151b010e4655"},
+        headers={"x-api-key": "4973b1ac-5e6b-4379-aa31-4a8ff80ba398"},
     )
     return {"message": res.json()}
